1d6e4f11ad99407ad40b0f39c40b54d667d3bcd6
guillaume <guillaume-huet@hotmail.com>
2021-01-05 14:29:36 +0100

    structure

---
+++ src/models/User.php
@@ -0,0 +1,10 @@
+<?php
+namespace mywishlist\models;
+class User extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table ='user';
+    protected $primaryKey ='id';
+    public $timestamps=false;
+
+
+}
