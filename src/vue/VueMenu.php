c64a35d408c2020a32de68c3b964b3e1bf14ff8c
FireKing54 <theo2001@laposte.net>
2021-01-20 18:16:50 +0100

    added a path for the search of a list from an ID

--- src/vue/VueMenu.php
+++ src/vue/VueMenu.php
@@ -67,6 +67,7 @@ END;
         $url_form_item = $container->router->pathFor( 'formItem' ,["uuid"=>'e35b86c734f614e2']) ;
         $url_form_login = $container->router->pathFor('formLogin');
         $url_test_login= $container->router->pathFor('testform');
+        $url_listes_rechercher = "#"; // $container->router->pathFor('formToken');
 
         $url_logo = 'https://www.carrefour.fr/media/540x540/Photosite/BAZAR/PAPETERIE/3037929416015_PHOTOSITE_20191115_050759_0.jpg?placeholder=1';
 
@@ -107,7 +108,10 @@ END;
 
                                 <div class="navbar-dropdown">
                                     <a class="navbar-item" href="$url_listes">
-                                        Listes
+                                        Listes publiques
+                                    </a>
+                                    <a class="navbar-item" href="$url_listes_rechercher">
+                                        Rechercher
                                     </a>
                                     <a class="navbar-item" href="$url_form_liste">
                                         Ajouter
